068e23f81c123569de270587ca04e0450ecf14a3
sumanthreddy0804 <gsumanth48@gmail.com>
2025-01-28 18:10:08 +0530

    files added

--- Modules/EC2_Module/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "web_server_new" {
-  ami           = var.ami_id
-  instance_type = var.Instance_type
-
-  tags = {
-    Name        = "Test_instance"
-    Environment = var.environment
-  }
-}
