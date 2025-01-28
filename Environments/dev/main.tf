641cc606d8e309403b334ff79033de5f19529a9e
sumanthreddy0804 <gsumanth48@gmail.com>
2025-01-28 17:56:33 +0530

    TF files added

--- Environments/dev/main.tf
+++ Environments/dev/main.tf
@@ -1,19 +1,19 @@
 module "ec2_instance" {
-  source           = "../EC2_Module/"
+  source           = "../Modules/EC2_Module/"
   ami_id           = var.ami_id
   Instance_type = var.instance_type
   environment = var.environment
 }
 
 module "s3_bucket" {
-  source       = "../S3_Module/"
+  source       = "../Modules/S3_Module/"
   bucket_name  = var.bucket_name
   acl       = var.acl
   environment  = var.environment
 }
 
 module "rds_instance" {
-  source              = "../RDS_Module/"
+  source              = "../Modules/RDS_Module/"
   db_identifier       = var.db_identifier
   db_allocated_storage = var.db_allocated_storage
   db_instance_class   = var.db_instance_class
