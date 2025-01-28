516e4f05a41fe3906858b1875366d01de32a7e64
Sudharshan12313 <167956857+Sudharshan12313@users.noreply.github.com>
2025-01-28 17:58:27 +0530

    Update backend.tf

--- Environments/staging/backend.tf
+++ Environments/staging/backend.tf
@@ -14,7 +14,7 @@ terraform {
 
   backend "s3" {
     bucket         = "nsh-state-new"
-    key            = "dev/terraform/key/terraform.tfstate"
+    key            = "staging/terraform/key/terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
   }
