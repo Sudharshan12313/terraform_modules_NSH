b13f70720c8538df8b04c1336fd7e7d3b48c492d
Sudharshan12313 <167956857+Sudharshan12313@users.noreply.github.com>
2025-01-29 09:07:34 +0530

    Update outputs.tf

--- Infra/Environments/prod/outputs.tf
+++ Infra/Environments/prod/outputs.tf
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-output "ec2_instance_id" {
+/*output "ec2_instance_id" {
   value = module.ec2_instance.instance_id
 }
 
@@ -18,5 +17,4 @@ output "bucket_name" {
 output "bucket_arn" {
   description = "The ARN of the S3 bucket"
   value       = aws_s3_bucket.static_content_bucket.arn
->>>>>>> 9162e1252b4a21bae7e5299ff615b8fd41c1d2d0
-}
+}*/
